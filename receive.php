885357cacfa8dfcb31ea3823273dba4ecd3ce5ac
kaoru <karlzoos0403@gmail.com>
2023-04-14 21:43:34 +0900

    入力フォームの確認

---
+++ receive.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>confirm</title>
+</head>
+<body>
+    <?php
+    echo $_POST[your_name];
+    ?>
+</body>
+</html>
