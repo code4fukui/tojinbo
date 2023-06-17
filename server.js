29f4326e6b4fbc3748ace4af829be5d1c5dc5aa8
84kaoru <128456055+84kaoru@users.noreply.github.com>
2023-06-17 18:06:52 +0900

    いいわんの表示

--- server.js
+++ server.js
@@ -6,12 +6,23 @@ const rock = new JSONDB("rock.json");
 
 serveAPI("/api/", async (param, req, path) => {
     if (path == "/api/save") {
+        param.id=crypto.randomUUID();
+        param.likecount=0;
         rock.data.push(param);
         rock.write();
         return "pushing data was saved"
     }
     if (path == "/api/watch") {
         return rock.data
+    }
+    if(path == "/api/like") {
+        console.log(param)
+        const d=rock.data.find(i => i.id==param.id);
+        console.log(d)
+        d.likecount=d.likecount+1;
+        console.log(d.likecount)
+        rock.write();
+        return "いいね+1"
     }
 
     return "岩投稿東尋坊"
